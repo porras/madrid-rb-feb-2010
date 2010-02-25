e8630cd5df4f4b3efcd5833192bbefe62b0bc4f0
Sergio Gil <sgilperez@gmail.com>
2010-02-25 18:41:21 +0100

    Configured Capybara to use Selenium

--- spec/acceptance/acceptance_helper.rb
+++ spec/acceptance/acceptance_helper.rb
@@ -3,8 +3,10 @@ require "steak"
 require "capybara"
 require "capybara/rails"
 require "capybara/dsl"
+require 'database_cleaner'
 
-Capybara.default_driver = :rack_test
+Capybara.default_driver = :selenium
+DatabaseCleaner.strategy = :truncation
 
 # Put your acceptance spec helpers inside /spec/acceptance/support
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
@@ -12,4 +14,13 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 Spec::Runner.configure do |config|
   config.include(Capybara)
   config.include(Factories)
+  
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
+
